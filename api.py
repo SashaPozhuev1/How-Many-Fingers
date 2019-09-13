9427d91a623d856dfbb22ab5650bcceefc7b5b17
SashaPozhuev1 <SashaPozh@ya.ru>
2019-09-13 18:15:44 +0300

    first change api.py

--- api.py
+++ api.py
@@ -1,6 +1,9 @@
 # coding: utf-8
 # Импортирует поддержку UTF-8.
 from __future__ import unicode_literals
+# Импортируем модули для игры
+import random
+import emoji
 
 # Импортируем модули для работы с JSON и логами.
 import json
@@ -16,11 +19,80 @@ logging.basicConfig(level=logging.DEBUG)
 # Хранилище данных о сессиях.
 sessionStorage = {}
 
+# Списки фраз для общения с игроком
+asks = [
+    'А сколько это пальцев?\n',
+    'Сколько пальцев на этот раз?\n',
+    'А сейчас?\n',
+    'Сколько пальцев я показываю?\n'
+]
+true_answers = [
+    'Правильно!\n',
+    'Молодец!\n',
+    'Угадал, попробуешь ещё?\n'
+]
+wrong_answers = [
+    'Нет!\n',
+    'Подумай еще...\n',
+    'Не угадал\n'
+]
+
+# Списки с различными эмоджи пальцев
+fingers_null = [
+    emoji.emojize(':raised_fist:'),
+    emoji.emojize(':oncoming_fist:'),
+    emoji.emojize(':left-facing_fist:'),
+    emoji.emojize(':right-facing_fist:')
+]
+fingers_one = [
+    emoji.emojize(':thumbs_down:'),
+    emoji.emojize(':thumbs_up:'),
+    emoji.emojize(':backhand_index_pointing_left:'),
+    emoji.emojize(':backhand_index_pointing_right:'),
+    emoji.emojize(':backhand_index_pointing_up:'),
+    emoji.emojize(':backhand_index_pointing_down:'),
+    emoji.emojize(':shushing_face:')
+]
+fingers_two = [
+    emoji.emojize(':victory_hand:'),
+    emoji.emojize(':crossed_fingers:'),
+    emoji.emojize(':sign_of_the_horns:'),
+    emoji.emojize(':call_me_hand:'),
+    emoji.emojize(':thinking_face:')
+]
+fingers_three = [
+    emoji.emojize(':love-you_gesture:'),
+]
+fingers_five = [
+    emoji.emojize(':clapping_hands:'),
+    emoji.emojize(':waving_hand:'),
+    emoji.emojize(':raised_back_of_hand:'),
+    emoji.emojize(':hand_with_fingers_splayed:'),
+    emoji.emojize(':raised_hand:'),
+    emoji.emojize(':vulcan_salute:'),
+    emoji.emojize(':face_with_hand_over_mouth:')
+]
+fingers_ten = [
+    emoji.emojize(':raising_hands:'),
+    emoji.emojize(':open_hands:'),
+    emoji.emojize(':palms_up_together:'),
+    emoji.emojize(':hugging_face:'),
+]
+other_emoji = [
+    emoji.emojize(':astonished_face:'),
+    emoji.emojize(':grinning_face:'),
+    emoji.emojize(':winking_face:'),
+    emoji.emojize(':face_blowing_a_kiss:'),
+    emoji.emojize(':winking_face_with_tongue:'),
+    emoji.emojize(':zipper-mouth_face:'),
+    emoji.emojize(':lying_face:'),
+    emoji.emojize(':sleeping_face:'),
+]
+
 # Задаем параметры приложения Flask.
 @app.route("/", methods=['POST'])
-
 def main():
-# Функция получает тело запроса и возвращает ответ.
+    # Функция получает тело запроса и возвращает ответ.
     logging.info('Request: %r', request.json)
 
     response = {
@@ -41,42 +113,116 @@ def main():
         indent=2
     )
 
+
 # Функция для непосредственной обработки диалога.
 def handle_dialog(req, res):
     user_id = req['session']['user_id']
 
     if req['session']['new']:
         # Это новый пользователь.
-        # Инициализируем сессию и поприветствуем его.
+        # Инициализируем сессию и поприветствуем его, показав правила игры
+        fingers_prev = random.randint(0, 10)
+        fingers_curr = random.randint(0, 10)
 
         sessionStorage[user_id] = {
             'suggests': [
                 "Не хочу.",
                 "Не буду.",
                 "Отстань!",
-            ]
+            ],
+            'fingers_prev': fingers_prev,
+            'fingers_curr': fingers_curr,
+            'count_fail': 0,
+            'count_success': 0
         }
 
-        res['response']['text'] = 'Привет! Купи слона!'
+        res['response']['text'] = 'Привет, давай сыграем!\n'
+        # Сохраняем данные о пальцах, добавляем правила
+        sessionStorage[user_id]['fingers_prev'] = fingers_curr
+        sessionStorage[user_id]['fingers_curr'] = start_game(res, fingers_prev, fingers_curr)
+
         res['response']['buttons'] = get_suggests(user_id)
         return
 
     # Обрабатываем ответ пользователя.
     if req['request']['original_utterance'].lower() in [
-        'ладно',
-        'куплю',
-        'покупаю',
-        'хорошо',
+        'один',
+        'два',
+        'три',
+        'четыре',
+        'пять',
+        'шесть',
+        'семь',
+        'восемь',
+        'девять',
+        'десять'
     ]:
-        # Пользователь согласился, прощаемся.
-        res['response']['text'] = 'Слона можно найти на Яндекс.Маркете!'
+        # Достаём данные из хранилища
+        fingers_prev = sessionStorage[user_id]['fingers_prev']
+        fingers_curr = sessionStorage[user_id]['fingers_curr']
+        count_success = sessionStorage[user_id]['count_success']
+        count_fail = sessionStorage[user_id]['count_fail']
+
+        # Проверяем ответ клиента
+        client_numb = -1
+        for obj in req['request']['nlu']['entities']:
+            if obj['type'] == 'YANDEX.NUMBER':
+                client_numb = obj['value']
+                break
+
+        if client_numb == fingers_prev:
+            res['response']['text'] = random.choice(true_answers)
+            count_fail = 0
+            count_success += 1
+        else:
+            res['response']['text'] = random.choice(wrong_answers) + ' это ' + str(fingers_prev) + ' пальцев\n'
+            count_fail += 1
+            count_success = 0
+
+        sessionStorage[user_id]['count_fail'] = count_fail
+        sessionStorage[user_id]['count_success'] = count_success
+        '''?!!!'''
+        '''if count_success == 4:
+            count_success = 0
+            res['response']['text'] += 'Ого, кажется ты смог разобраться в этой игре! Прими мои поздравления!\n'''
+        # Задаём новый вопрос
+        if count_fail == 6:
+            fingers_prev = random.randint(0, 10)
+            fingers_curr = random.randint(0, 10)
+            sessionStorage[user_id]['fingers_prev'] = fingers_curr
+            sessionStorage[user_id]['fingers_curr'] = start_game(res, fingers_prev, fingers_curr)
+            sessionStorage[user_id]['count_fail'] = 0
+            sessionStorage[user_id]['count_success'] = 0
+            return
+        else:
+            fingers_prev = fingers_curr
+            fingers_curr = random.randint(0, 10)
+            res['response']['text'] += random.choice(asks)
+            if random.randint(0, 2) == 0:
+                res['response']['text'] += random.choice(other_emoji)
+            res['response']['text'] += get_fingers(fingers_curr)
+            sessionStorage[user_id]['fingers_prev'] = fingers_prev
+            sessionStorage[user_id]['fingers_curr'] = fingers_curr
+
         return
 
     # Если нет, то убеждаем его купить слона!
-    res['response']['text'] = 'Все говорят "%s", а ты купи слона!' % (
+    '''просим ввести правильный ответ'''
+    res['response']['text'] = 'Все говорят "%s", а ты введи число' % (
         req['request']['original_utterance']
     )
     res['response']['buttons'] = get_suggests(user_id)
+
+
+# Демонстрирует правила
+def start_game(res, fingers_prev, fingers_curr):
+    fingers_next = random.randint(0, 10)
+    res['response']['text'] += 'Смотри, это ' + get_fingers(fingers_prev) + ' - ' + str(fingers_prev) + ' пальцев.\n' + \
+                               'и это: ' + get_fingers(fingers_curr) + ' - ' + str(fingers_prev) + ' пальцев\n' +\
+                               'А сколько это пальцев? ' + get_fingers(fingers_next)
+
+    return fingers_next
+
 
 # Функция возвращает две подсказки для ответа.
 def get_suggests(user_id):
@@ -102,3 +248,30 @@ def get_suggests(user_id):
         })
 
     return suggests
+
+
+def get_fingers(fingers):
+    if fingers == 0:
+        return random.choice(fingers_null)
+    elif fingers == 1:
+        return random.choice(fingers_one)
+    elif fingers == 2:
+        return random.choice(fingers_two)
+    elif fingers == 3:
+        return random.choice(fingers_three)
+    elif fingers == 4:
+        return random.choice(fingers_three) + random.choice(fingers_one)
+    elif fingers == 5:
+        return random.choice(fingers_five)
+    elif fingers == 6:
+        return random.choice(fingers_five) + random.choice(fingers_one)
+    elif fingers == 7:
+        return random.choice(fingers_five) + random.choice(fingers_two)
+    elif fingers == 8:
+        return random.choice(fingers_five) + random.choice(fingers_three)
+    elif fingers == 9:
+        return random.choice(fingers_five) + random.choice(fingers_three) + random.choice(fingers_one)
+    elif fingers == 10:
+        return random.choice(fingers_ten)
+    return False
+
